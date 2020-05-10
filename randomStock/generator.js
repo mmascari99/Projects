83e9df341f570e84629a9926118ee0b07441ec7f
Michael Mascari <mascarimichael71@gmail.com>
2020-05-09 23:19:08 -0500

    Delete generator.js

--- randomStock/generator.js
+++
@@ -1,9 +0,0 @@
-//picks a random stock ticker and a random direction then prints to website "'ticker' will go 'direction'"
-function generate(){
-    var ticker = ["SPY", "MSFT", "NFLX", "AMD", "APPL", "SNAP", "TWTR", "FB", "DIS", "F", "BABA", "GE", "TSLA", "MCD", "CGC"]
-    var tendies = ["up", "down"]
-	  var random = Math.floor((Math.random( ) * 15)) % 15; 
-	  var direction = ((Math.floor(Math.random() * 2)) %2);
-    var newstring = "$" + ticker[random] + " will go " + tendies[direction];
-    document.getElementById("content").innerHTML = newstring;
-}	
